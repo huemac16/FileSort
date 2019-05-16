05f4fcf70d62d04e70c369b7a906a94377f803e4
huemac16 <huemac16@htl-kaindorf.ac.at>
2019-05-16 21:30:39 +0200

    added pic

--- FileSort/src/GUI/ChooseDirectory.java
+++ FileSort/src/GUI/ChooseDirectory.java
@@ -1,36 +1,87 @@
 package GUI;
 
-import javax.swing.JFileChooser;
+import java.io.File;
+import javax.swing.JOptionPane;
 
 public class ChooseDirectory extends javax.swing.JDialog {
+
+    private File directory;
+
+    public File getDirectory() {
+        return directory;
+    }
 
     public ChooseDirectory(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
-        jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        pnBack = new javax.swing.JPanel();
+        btSelect = new javax.swing.JButton();
         jFileChooser = new javax.swing.JFileChooser();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("DirectoryChooser");
+
+        btSelect.setText("Select directory");
+        btSelect.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btSelectActionPerformed(evt);
+            }
+        });
+
+        jFileChooser.setControlButtonsAreShown(false);
+
+        javax.swing.GroupLayout pnBackLayout = new javax.swing.GroupLayout(pnBack);
+        pnBack.setLayout(pnBackLayout);
+        pnBackLayout.setHorizontalGroup(
+            pnBackLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jFileChooser, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(pnBackLayout.createSequentialGroup()
+                .addGap(51, 51, 51)
+                .addComponent(btSelect, javax.swing.GroupLayout.PREFERRED_SIZE, 502, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(52, Short.MAX_VALUE))
+        );
+        pnBackLayout.setVerticalGroup(
+            pnBackLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnBackLayout.createSequentialGroup()
+                .addComponent(jFileChooser, javax.swing.GroupLayout.DEFAULT_SIZE, 349, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(btSelect)
+                .addGap(27, 27, 27))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jFileChooser, javax.swing.GroupLayout.DEFAULT_SIZE, 582, Short.MAX_VALUE)
+            .addComponent(pnBack, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jFileChooser, javax.swing.GroupLayout.DEFAULT_SIZE, 373, Short.MAX_VALUE)
+            .addComponent(pnBack, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btSelectActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btSelectActionPerformed
+        try {
+            directory = jFileChooser.getCurrentDirectory();
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Please choose a valid directory!");
+        }
+        System.out.println("" + directory.getAbsolutePath());
+
+        dispose();
+
+    }//GEN-LAST:event_btSelectActionPerformed
 
     /**
      * @param args the command line arguments
@@ -75,6 +126,8 @@ public class ChooseDirectory extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btSelect;
     private javax.swing.JFileChooser jFileChooser;
+    private javax.swing.JPanel pnBack;
     // End of variables declaration//GEN-END:variables
 }
